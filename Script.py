78482e6de709ad5263ef9ccb0bae010f7f39d198
Kamil Baranowski <kamilbaranowski85@gmail.com>
2020-10-06 21:36:13 +0200

    Remove code

--- Script.py
+++ Script.py
@@ -1,13 +1,11 @@
 # Sort Import
 import math
-import sys
-from os import rename
 
 # import request
 
 # CTRL + SHIFT + I
-print(sys.version)
-print(sys.executable)
 
 name = input("Your name? ")
 print("hello,", name)
+
+print(r.ok)
