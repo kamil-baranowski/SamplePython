2430227d1f50e09b0b2871b471261c37d2fbe901
Kamil Baranowski <kamilbaranowski85@gmail.com>
2020-10-06 21:31:40 +0200

    Initial Commit

---
+++ Script.py
@@ -0,0 +1,13 @@
+# Sort Import
+import math
+import sys
+from os import rename
+
+# import request
+
+# CTRL + SHIFT + I
+print(sys.version)
+print(sys.executable)
+
+name = input("Your name? ")
+print("hello,", name)
